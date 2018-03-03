8a9fc9c8411f2ed3ca2f412a51f52d22fdbc1987
Danie Britto Kuthalane <dannybritto96@gmail.com>
2018-03-03 11:42:25 +0530

    Update bank.py

--- bank/bank.py
+++ bank/bank.py
@@ -1,14 +1,13 @@
+
 import numpy as np
 import pandas as pd
-from keras.wrappers.scikit_learn import KerasClassifier
-from keras.layers import Dense, Dropout
-from keras.models import Sequential
-from sklearn.model_selection import cross_val_score
+from sklearn import tree
+from IPython.display import Image
+from sklearn.externals.six import StringIO
+from sklearn.ensemble import RandomForestClassifier
+import pydotplus
 
-feature_names = ['age','job','martial','education','default','balance','housing','loan','contact','day','month','duration','campaign','pdays','previous','poutcome','y']
-dataset = pd.read_csv('bank.csv',sep=';',names = feature_names)
-dataset.dropna(inplace=True)
-dataset.replace(('yes','no'),(1,0), inplace=True)
+dataset = pd.read_csv('/Users/danny/Desktop/Practice Deep Learning/bank/bank.csv',sep=';',names=['age','job','martial','education','default','balance','housing','loan','contact','day','month','duration','campaign','pdays','previous','poutcome','y'])
 dataset.replace(('unknown'),('0'),inplace=True)
 dataset.replace(('married','divorced','single'),(1,2,3),inplace=True)
 dataset.replace(('admin.','blue-collar','entrepreneur','housemaid','management','retired','self-employed','services','student','technician','unemployed'),(1,2,3,4,5,6,7,8,9,10,11),inplace=True)
@@ -16,22 +15,25 @@ dataset.replace(('primary','secondary','tertiary'),(1,2,3),inplace=True)
 dataset.replace(('cellular','telephone'),(1,2),inplace=True)
 dataset.replace(('jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'),(0,1,2,3,4,5,6,7,8,9,10,11),inplace=True)
 dataset.replace(('failure','nonexistent','success','other'),(1,2,3,4),inplace=True)
+dataset.replace(('yes','no'),(1,0), inplace=True)
 
-all_features = dataset[feature_names].drop('y',axis=1).values
-all_classes = dataset['y'].values
+features = list(dataset.columns[:16])
+y = dataset['y']
+X = dataset[features]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X,y)
 
-def create_model():
-    model = Sequential()
-    model.add(Dense(32, input_dim=16, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(16, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(8, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(4, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(2, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-    return model
-    
-estimator = KerasClassifier(build_fn=create_model, nb_epoch=100, verbose=1)
-cv_scores = cross_val_score(estimator, all_features, all_classes, cv=10)
-cv_scores.mean()
+dot_data = StringIO()  
+tree.export_graphviz(clf, out_file=dot_data,  
+                         feature_names=features)  
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+Image(graph.create_png())
+
+clf = RandomForestClassifier(n_estimators=10)
+clf = clf.fit(X, y)
+
+
+
+print(clf.predict([[30,11,1,1,0,1787,0,0,1,19,9,79,1,1,0,0]]))
+print(clf.predict([[68,6,2,2,0,4189,0,0,2,14,6,897,2,1,0,0]]))
 
