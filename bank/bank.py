5f2e132fe89244b5a488dbc99d264ff542010276
Danie Britto Kuthalane <dannybritto96@gmail.com>
2018-03-12 09:56:07 +0530

    Update bank.py

--- bank/bank.py
+++ bank/bank.py
@@ -7,6 +7,8 @@ from sklearn.externals.six import StringIO
 from sklearn.ensemble import RandomForestClassifier
 import pydotplus
 
+# Loading data into Pandas Dataframe and Data preprocessing phase
+
 dataset = pd.read_csv('/Users/danny/Desktop/Practice Deep Learning/bank/bank.csv',sep=';',names=['age','job','martial','education','default','balance','housing','loan','contact','day','month','duration','campaign','pdays','previous','poutcome','y'])
 dataset.replace(('unknown'),('0'),inplace=True)
 dataset.replace(('married','divorced','single'),(1,2,3),inplace=True)
@@ -16,6 +18,8 @@ dataset.replace(('cellular','telephone'),(1,2),inplace=True)
 dataset.replace(('jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'),(0,1,2,3,4,5,6,7,8,9,10,11),inplace=True)
 dataset.replace(('failure','nonexistent','success','other'),(1,2,3,4),inplace=True)
 dataset.replace(('yes','no'),(1,0), inplace=True)
+
+# Decision Trees
 
 features = list(dataset.columns[:16])
 y = dataset['y']
@@ -33,6 +37,7 @@ clf = RandomForestClassifier(n_estimators=10)
 clf = clf.fit(X, y)
 
 
+# Make new predictions
 
 print(clf.predict([[30,11,1,1,0,1787,0,0,1,19,9,79,1,1,0,0]]))
 print(clf.predict([[68,6,2,2,0,4189,0,0,2,14,6,897,2,1,0,0]]))
